a1352d78d8d05d8fbd8f2944285b8911cb895d12
whgusals <262mee@dimigo.hs.kr>
2026-09-25 09:49:30 +0900

    Fix analytics lint and privacy verification

--- public/assets/site.js
+++ public/assets/site.js
@@ -83,8 +83,8 @@
     if (document.querySelector('[data-pixn-analytics]')) return;
 
     window.dataLayer = window.dataLayer || [];
-    window.gtag = window.gtag || function gtag() {
-      window.dataLayer.push(arguments);
+    window.gtag = window.gtag || function gtag(...args) {
+      window.dataLayer.push(args);
     };
     window.gtag('consent', 'default', {
       analytics_storage: 'granted',
